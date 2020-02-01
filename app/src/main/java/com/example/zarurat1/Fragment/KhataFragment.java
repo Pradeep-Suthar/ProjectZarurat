7ed2783f3123f30da4d027ee1c31fe19b77552d5
Pradeep <pradeepsuthar800@gmial.com>
2020-02-01 11:15:49 +0530

    change

--- app/src/main/java/com/example/zarurat1/Fragment/KhataFragment.java
+++ app/src/main/java/com/example/zarurat1/Fragment/KhataFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.zarurat1.R;
 
 public class KhataFragment extends Fragment {
+    TextView textView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
