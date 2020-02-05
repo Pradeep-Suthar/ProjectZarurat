2d844b6636b0a21db4726bf2962ecb0e6ec77c31
Pradeep <pradeepsuthar800@gmial.com>
2020-02-05 22:39:26 +0530

    changes1

--- app/src/main/java/com/example/zarurat1/Activity/PersonalNewsListActivity.java
+++ app/src/main/java/com/example/zarurat1/Activity/PersonalNewsListActivity.java
@@ -2,14 +2,121 @@ package com.example.zarurat1.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.zarurat1.Adapter.NewsCustomAdapter;
+import com.example.zarurat1.Pojo.NewsPojo;
 import com.example.zarurat1.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class PersonalNewsListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private ProgressBar progressBar;
+    ArrayList<NewsPojo> userPojos = new ArrayList<>();
+
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personal_news_list);
+
+        String newstype=getIntent().getStringExtra("newsType");
+
+        Toolbar toolbar1 = findViewById(R.id.toolbar1);
+        setSupportActionBar(toolbar1);
+        getSupportActionBar().setTitle(newstype+" News");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+
+
+
+        final String url = "https://newsapi.org/v2/top-headlines?country=in&category="+newstype+"&apiKey=33f92e4321d34c3d91ea41e6df82269c";
+
+        toolbar1.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+        recyclerView = findViewById(R.id.recyclerviewPresonalNews);
+        recyclerView.setHasFixedSize(true);
+        progressBar= findViewById(R.id.progressBarPer);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        Log.d("1234", "onCreate: ");
+        requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Log.d("1234", "onResponse: "+response);
+
+                try {
+                    JSONObject object = new JSONObject(response);
+                    JSONArray array = object.getJSONArray("articles");
+
+                    for (int i = 0; i < array.length(); i++){
+                        JSONObject jsonObject = array.getJSONObject(i);
+                        NewsPojo newsPojo = new NewsPojo();
+                        newsPojo.setName(jsonObject.getJSONObject("source").getString("name"));
+                        newsPojo.setTitle(jsonObject.getString("title"));
+                        newsPojo.setDescription(jsonObject.getString("description"));
+                        newsPojo.setLink(jsonObject.getString("url"));
+                        newsPojo.setImgUrl(jsonObject.getString("urlToImage"));
+                        newsPojo.setDate(jsonObject.getString("publishedAt"));
+                        newsPojo.setContent(jsonObject.getString("content"));
+                        userPojos.add(newsPojo);
+                    }
+
+                    Log.d("12345", "onResponse: getview ");
+                    NewsCustomAdapter newsCustomAdapter = new NewsCustomAdapter(PersonalNewsListActivity.this, userPojos);
+                    progressBar.setVisibility(View.GONE);
+                    recyclerView.setAdapter(newsCustomAdapter);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("1234", "onErrorResponse: "+error.getMessage());
+
+                Toast.makeText(PersonalNewsListActivity.this, "Network Error", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        Log.d("1234", "onCreate: 123");
+        requestQueue.add(request);
+
     }
 }
--- app/src/main/java/com/example/zarurat1/Fragment/PersonlizedNewsFragment.java
+++ app/src/main/java/com/example/zarurat1/Fragment/PersonlizedNewsFragment.java
@@ -1,22 +1,92 @@
 package com.example.zarurat1.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.zarurat1.Activity.PersonalNewsListActivity;
 import com.example.zarurat1.R;
 
 public class PersonlizedNewsFragment extends Fragment {
+    CardView cardViewEntertainment,cardViewBuisness,cardViewSports,cardViewHealth,cardViewScience,cardViewTechnology;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personlized_news, container, false);
+        final View view= inflater.inflate(R.layout.fragment_personlized_news, container, false);
+        cardViewBuisness=view.findViewById(R.id.cardBuisness);
+        cardViewEntertainment=view.findViewById(R.id.cardEntertainment);
+        cardViewScience=view.findViewById(R.id.cardScience);
+        cardViewHealth=view.findViewById(R.id.cardHealth);
+        cardViewSports=view.findViewById(R.id.cardSports);
+        cardViewTechnology=view.findViewById(R.id.cardTechnology);
+
+        cardViewTechnology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), PersonalNewsListActivity.class);
+                intent.putExtra("newsType","Technology");
+                startActivity(intent);
+
+            }
+        });
+
+        cardViewSports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), PersonalNewsListActivity.class);
+                intent.putExtra("newsType","Sports");
+                startActivity(intent);
+
+            }
+        });
+
+
+        cardViewHealth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), PersonalNewsListActivity.class);
+                intent.putExtra("newsType","Health");
+                startActivity(intent);
+            }
+        });
+
+
+        cardViewScience.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), PersonalNewsListActivity.class);
+                intent.putExtra("newsType","Science");
+                startActivity(intent);
+            }
+        });
+
+
+        cardViewEntertainment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), PersonalNewsListActivity.class);
+                intent.putExtra("newsType","Entertainment");
+                startActivity(intent);
+            }
+        });
+
+
+        cardViewBuisness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(), PersonalNewsListActivity.class);
+                intent.putExtra("newsType","business");
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 
 }
