b13cc3ceaf3bc1d850522429b4be8d2412db2252
Pradeep <pradeepsuthar800@gmial.com>
2020-02-05 14:36:08 +0530

    changes1

--- app/src/main/java/com/example/zarurat1/Activity/PersonalNewsListActivity.java
+++ app/src/main/java/com/example/zarurat1/Activity/PersonalNewsListActivity.java
@@ -1,7 +1,9 @@
-package com.example.zarurat1;
+package com.example.zarurat1.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.zarurat1.R;
 
 public class PersonalNewsListActivity extends AppCompatActivity {
 
