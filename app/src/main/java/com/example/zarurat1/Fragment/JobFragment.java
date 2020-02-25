d9ee509bee4db40216990e39cf2ea70ebd179874
Mukul <sharmamukul2598@gmail.com>
2020-02-25 19:26:21 +0530

    job fragment

--- app/src/main/java/com/example/zarurat1/Fragment/JobFragment.java
+++ app/src/main/java/com/example/zarurat1/Fragment/JobFragment.java
@@ -1,19 +1,19 @@
 package com.example.zarurat1.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.zarurat1.R;
 
 public class JobFragment extends Fragment {
     FloatingActionButton addbutton;
+    LinearLayout linearLayoutbottomsheet;
 
 
     @Override
@@ -22,13 +22,17 @@ public class JobFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_job, container, false);
         addbutton=view.findViewById(R.id.fab);
+        linearLayoutbottomsheet=view.findViewById(R.id.bottom_sheet);
+
+        final BottomSheetBehavior bottomSheetBehavior=BottomSheetBehavior.from(linearLayoutbottomsheet);
 
         addbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
         });
+
         return view;
 
 
